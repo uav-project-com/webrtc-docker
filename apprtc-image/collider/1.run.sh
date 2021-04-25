3abbd5b212b8ad9882fcaca78e1be9654a5b270f
Than Trung Hieu <hieu19926@gmail.com>
2021-04-25 17:41:11 +0700

    fix collider img

--- apprtc-image/collider/1.run.sh
+++ apprtc-image/collider/1.run.sh
@@ -4,9 +4,8 @@ docker run --name collider -p 2223:2222 -p 9443:9443 -d -it \
 	-e PASSWORD_ACCESS=true \
 	-e USER_PASSWORD=canhthong \
 	-e USER_NAME=admin \
-	-e HTTPS_ROOM_URL=https://phicongtre.site:5443 \
-	-e SSL_PORT=9443 \
-  -v ./key.key:/cert/key.pem \
-	-v ./cert.crt:/cert/cert.pem \
+	-e HTTPS_ROOM_URL=https://phicongtre.site:8443 \
+  -v ${PWD}/cert/key.pem:/cert/key.pem \
+	-v ${PWD}/cert/cert.pem:/cert/cert.pem \
 	nobjta9xtq/collider:1.0
 
